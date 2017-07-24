25495b833801a3d51483cb3dc7c38403f727efdc
Jeries Handal <JeriesHG@gmail.com>
2017-07-23 22:16:58 -0600

    Added calendar widget construction

--- calendar.js
+++ calendar.js
@@ -1,9 +1,9 @@
 function CalendarController($scope) {
-  var monthNames = ["January", "February", "March", "April", "May", "June",
+  $scope.monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
   ];
-
-  $scope.calendar = {};
+  
+  $scope.calendar = [];
   $scope.search = {
     startDate: new Date('2017/07/23'),
     days: 2,
@@ -12,21 +12,33 @@ function CalendarController($scope) {
 
   $scope.buildCalendar = function() {
     var startDate = new Date($scope.search.startDate);
-    var addedDays = addDays(new Date($scope.search.startDate), $scope.search.days);
+    var widgets = retrieveWidgets(
+      startDate,
+      addDays(new Date($scope.search.startDate), $scope.search.days)
+    );
 
-    var widgets = monthsDifference(startDate, addedDays);
+    for (var i = 0; i < widgets; i++) {
+      var currentMonth = new Date(startDate.setMonth(+startDate.getMonth() + i));
+      var widget = {
+        header: `${$scope.monthNames[currentMonth.getMonth()]} ${currentMonth.getFullYear()}`
 
+      }
 
+      $scope.calendar.push(widget);
+    }
+
+    console.log($scope.calendar);
   };
 
-  function monthsDifference(d1, d2) {
+  function retrieveWidgets(d1, d2) {
     var months;
 
     months = (d2.getFullYear() - d1.getFullYear()) * 12;
     months -= d1.getMonth() + 1;
     months += d2.getMonth();
 
-    return months <= 0 ? 0 : months;
+
+    return months < 0 ? 1 : (months + 2);
   }
 
   function addDays(date, days) {
